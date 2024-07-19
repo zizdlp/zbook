c37c26dc4eedb4ff4beb2770c54d69c6c5279928
SeeLey Wang <zdlp@sina.cn>
2024-07-19 19:46:58 +0800

    fix layout generate

--- zbook_backend/operations/list_markdowns.go
+++ zbook_backend/operations/list_markdowns.go
@@ -9,8 +9,9 @@ import (
 // ListMarkdownFiles lists all .md files in the git repository located at the specified directory.
 func ListMarkdownFiles(repoDir string) ([]string, error) {
 	// Construct the git ls-files command
-	cmd := exec.Command("git", "-C", repoDir, "ls-files")
-
+	// cmd := exec.Command("git", "-C", repoDir, "ls-files")
+	cmd := exec.Command("sh", "-c", "git ls-files -z | xargs -0 -n1 echo")
+	cmd.Dir = repoDir // Set the working directory to the specified repository directory
 	// Run the command and capture its output
 	output, err := cmd.Output()
 	if err != nil {
