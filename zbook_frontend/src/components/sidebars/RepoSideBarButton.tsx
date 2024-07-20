0e121468aaf3f71a7b3afdd8842717f2d0a84f50
SeeLey Wang <zdlp@sina.cn>
2024-07-20 14:27:06 +0800

    add cursor pointer

--- zbook_frontend/src/components/sidebars/RepoSideBarButton.tsx
+++ zbook_frontend/src/components/sidebars/RepoSideBarButton.tsx
@@ -17,7 +17,7 @@ export default function RepoSideBarButton({
       <Link
         onClick={onclick}
         href={url}
-        className="group flex items-center lg:leading-6 mb-5 sm:mb-4  text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300"
+        className="group flex items-center lg:leading-6 mb-5 sm:mb-4  text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer"
       >
         <div
           className={`mr-4 rounded-md p-1 zinc-box group-hover:brightness-100 group-hover:ring-0 ring-1 ring-gray-950/5 dark:ring-gray-700/40 ${className}`}
@@ -31,7 +31,7 @@ export default function RepoSideBarButton({
     return (
       <div
         onClick={onclick}
-        className="group flex items-center lg:leading-6 mb-5 sm:mb-4  text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300"
+        className="group flex items-center lg:leading-6 mb-5 sm:mb-4  text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300 cursor-pointer"
       >
         <div
           className={`mr-4 rounded-md p-1 zinc-box group-hover:brightness-100 group-hover:ring-0 ring-1 ring-gray-950/5 dark:ring-gray-700/40 ${className}`}
