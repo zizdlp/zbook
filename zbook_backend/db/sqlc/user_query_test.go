d6ee0e73fa86316ed1af6bf3d06fdbbafed1cc7a
SeeLey Wang <zdlp@sina.cn>
2024-07-23 20:25:21 +0800

    a

--- zbook_backend/db/sqlc/user_query_test.go
+++ zbook_backend/db/sqlc/user_query_test.go
@@ -35,13 +35,75 @@ func TestGetUserByEmail(t *testing.T) {
 
 func TestGetUnreadCount(t *testing.T) {
 	user1 := createRandomUser(t)
+	user2 := createRandomUser(t)
+	arg_follow := CreateFollowTxParams{
+		CreateFollowParams: CreateFollowParams{FollowerID: user2.UserID, FollowingID: user1.UserID},
+	}
+	_, err := testStore.CreateFollowTx(context.Background(), arg_follow)
+	require.NoError(t, err)
 	testStore.UpdateUnreadCount(context.Background(), user1.UserID)
 	unreadCount, err := testStore.GetUnReadCount(context.Background(), user1.Username)
 	require.NoError(t, err)
-	require.Equal(t, user1.UnreadCount, unreadCount)
+	require.Equal(t, int32(1), unreadCount)
+}
+
+func TestListUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2 := createRandomUser(t)
+	user3 := createRandomUser(t)
+
+	arg := ListUserParams{
+		Limit:  3,
+		Offset: 0,
+	}
+	users, err := testStore.ListUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.Equal(t, len(users), 3)
+	require.Equal(t, user3.UserID, users[0].UserID)
+	require.Equal(t, user2.UserID, users[1].UserID)
+	require.Equal(t, user1.UserID, users[2].UserID)
+}
+
+func TestGetListUserCount(t *testing.T) {
+
+	count1, err := testStore.GetListUserCount(context.Background())
+	require.NoError(t, err)
+	createRandomUser(t)
+	count2, err := testStore.GetListUserCount(context.Background())
+	require.NoError(t, err)
+	require.Equal(t, count2, count1+1)
+}
+
+func TestQueryUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	arg := QueryUserParams{
+		Limit:  10,
+		Offset: 0,
+		Query:  user1.Username,
+	}
+	users, err := testStore.QueryUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, users)
+	require.Equal(t, users[0].UserID, user1.UserID)
+}
+func TestGetQueryUserCount(t *testing.T) {
+	user1 := createRandomUser(t)
+	count, err := testStore.GetQueryUserCount(context.Background(), user1.Username)
+	require.NoError(t, err)
+	require.True(t, count > 0)
+}
+func TestDailyCreateUserCount(t *testing.T) {
+	createRandomUser(t)
+	counts, err := testStore.GetDailyCreateUserCount(context.Background())
+	require.NoError(t, err)
+	require.True(t, len(counts) > 0)
 }
 
 func TestGetUserInfoByID(t *testing.T) {
+	// user1:4 repo
+	// user2:4 repo
+	// user3:4 repo,blocked
+
 	user1 := createRandomUser(t)
 	user2 := createRandomUser(t)
 	user3 := createRandomUser(t)
@@ -76,9 +138,11 @@ func TestGetUserInfoByID(t *testing.T) {
 	updateRepoVisibility(t, repo34, util.VisibilitySigned)
 
 	testCreateRelationUserRepoRelation(t, user1, repo23, util.RelationTypeDislike)
-	testCreateRelationUserRepoRelation(t, user1, repo34, util.RelationTypeLike)
-	testCreateRelationUserRepoRelation(t, user1, repo11, util.RelationTypeLike)
-	testCreateRelationUserRepoRelation(t, user1, repo14, util.RelationTypeLike)
+	testCreateRelationUserRepoRelation(t, user1, repo34, util.RelationTypeLike) //like +1
+	testCreateRelationUserRepoRelation(t, user1, repo11, util.RelationTypeLike) //like +1
+	testCreateRelationUserRepoRelation(t, user1, repo14, util.RelationTypeLike) //like +1
+
+	// user2 see user1:
 	arg := GetUserInfoParams{
 		CurUserID: user2.UserID,
 		UserID:    user1.UserID,
@@ -91,71 +155,40 @@ func TestGetUserInfoByID(t *testing.T) {
 	require.Equal(t, user_info.RepoCount, int64(2))
 	require.Equal(t, user_info.LikeCount, int64(2))
 	require.Equal(t, user_info.IsFollowing, false)
-}
 
-func TestListUser(t *testing.T) {
-	user1 := createRandomUser(t)
-	user2 := createRandomUser(t)
-	user3 := createRandomUser(t)
-	repo11 := createUserRandomRepo(t, user1)
-	repo12 := createUserRandomRepo(t, user1)
-	repo13 := createUserRandomRepo(t, user1)
-	repo14 := createUserRandomRepo(t, user1)
-
-	updateRepoVisibility(t, repo11, util.VisibilityChosed)
-	updateRepoVisibility(t, repo12, util.VisibilityPrivate)
-	updateRepoVisibility(t, repo13, util.VisibilityPublic)
-	updateRepoVisibility(t, repo14, util.VisibilitySigned)
-
-	repo21 := createUserRandomRepo(t, user2)
-	repo22 := createUserRandomRepo(t, user2)
-	repo23 := createUserRandomRepo(t, user2)
-	repo24 := createUserRandomRepo(t, user2)
-
-	updateRepoVisibility(t, repo21, util.VisibilityChosed)
-	updateRepoVisibility(t, repo22, util.VisibilityPrivate)
-	updateRepoVisibility(t, repo23, util.VisibilityPublic)
-	updateRepoVisibility(t, repo24, util.VisibilitySigned)
-
-	repo31 := createUserRandomRepo(t, user3)
-	repo32 := createUserRandomRepo(t, user3)
-	repo33 := createUserRandomRepo(t, user3)
-	repo34 := createUserRandomRepo(t, user3)
-
-	updateRepoVisibility(t, repo31, util.VisibilityChosed)
-	updateRepoVisibility(t, repo32, util.VisibilityPrivate)
-	updateRepoVisibility(t, repo33, util.VisibilityPublic)
-	updateRepoVisibility(t, repo34, util.VisibilitySigned)
-
-	testCreateRelationUserRepoRelation(t, user1, repo23, util.RelationTypeDislike)
-	testCreateRelationUserRepoRelation(t, user1, repo34, util.RelationTypeLike)
-	testCreateRelationUserRepoRelation(t, user1, repo11, util.RelationTypeLike)
-	testCreateRelationUserRepoRelation(t, user1, repo14, util.RelationTypeLike)
-	arg := ListUserParams{
-		Limit:  1000,
-		Offset: 0,
+	// user1 see user1:
+	arg = GetUserInfoParams{
+		CurUserID: user1.UserID,
+		UserID:    user1.UserID,
+		Signed:    true,
+		Role:      user1.UserRole,
 	}
-	users, err := testStore.ListUser(context.Background(), arg)
+	user_info, err = testStore.GetUserInfo(context.Background(), arg)
 	require.NoError(t, err)
-	require.True(t, len(users) >= 3)
+	require.Equal(t, user_info.UserID, user1.UserID)
+	require.Equal(t, user_info.RepoCount, int64(4))
+	require.Equal(t, user_info.LikeCount, int64(3))
+	require.Equal(t, user_info.IsFollowing, false)
 
-}
+	// update user3 to block
+	arg_block := UpdateUserBasicInfoParams{
+		Username: user3.Username,
+		Blocked:  pgtype.Bool{Bool: true, Valid: true},
+	}
+	_, err = testStore.UpdateUserBasicInfo(context.Background(), arg_block)
+	require.NoError(t, err)
 
-func TestQueryUser(t *testing.T) {
-	user1 := createRandomUser(t)
-	arg_update := UpdateUserBasicInfoParams{
-		Username: user1.Username,
-		Verified: pgtype.Bool{Bool: true, Valid: true},
+	arg = GetUserInfoParams{
+		CurUserID: user1.UserID,
+		UserID:    user1.UserID,
+		Signed:    true,
+		Role:      user1.UserRole,
 	}
-	_, err := testStore.UpdateUserBasicInfo(context.Background(), arg_update)
+	user_info, err = testStore.GetUserInfo(context.Background(), arg)
 	require.NoError(t, err)
-	arg := QueryUserParams{
-		Limit:  10,
-		Offset: 0,
-		Query:  user1.Username,
-	}
-	users, err := testStore.QueryUser(context.Background(), arg)
-	require.NoError(t, err)
-	require.NotEmpty(t, users)
-	require.Equal(t, users[0].UserID, user1.UserID)
+	require.Equal(t, user_info.UserID, user1.UserID)
+	require.Equal(t, user_info.RepoCount, int64(4))
+	require.Equal(t, user_info.LikeCount, int64(2)) //block one
+	require.Equal(t, user_info.IsFollowing, false)
+
 }
