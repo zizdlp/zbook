e2ab263436495bbf87851b46ebb19277048a08d4
SeeLey Wang <zdlp@sina.cn>
2024-07-28 10:02:14 +0800

    a

--- zbook_frontend/src/components/elements/details/RepoButtons.tsx
+++ zbook_frontend/src/components/elements/details/RepoButtons.tsx
@@ -15,8 +15,13 @@ export default function RepoButtons({
   authname: string;
   repo_id: number;
 }) {
-  const { setUpdateRepoOpen, setOperationRepoID, setDeleteRepoOpen } =
-    useContext(OperationContext);
+  const {
+    setUpdateRepoOpen,
+    setDeleteRepoOpen,
+    setOperationRepoID,
+    setOperationUsername,
+    setOperationRepoName,
+  } = useContext(OperationContext);
   if (authname != username) {
     console.log("authname:", authname, username);
     return <></>;
@@ -32,7 +37,8 @@ export default function RepoButtons({
       />
       <MdDeleteOutline
         onClick={() => {
-          setOperationRepoID(repo_id);
+          setOperationRepoName(reponame);
+          setOperationUsername(username);
           setDeleteRepoOpen(true);
         }}
         className="p-1 w-7 h-7 cursor-pointer text-gray-500 bg-gray-200 rounded dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-900 dark:text-gray-400"
