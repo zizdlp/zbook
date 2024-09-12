ec05f3a1d75d3f88619489a44f77104a37295ba3
SeeLey Wang <37832104+zizdlp@users.noreply.github.com>
2024-09-12 21:33:26 +0800

    additional opertions for lang de support

--- zbook_frontend/src/navigation.ts
+++ zbook_frontend/src/navigation.ts
@@ -1,5 +1,5 @@
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
-export const locales = ["en", "zh"] as const;
+export const locales = ["en", "zh", "de"] as const;
 export const { Link, redirect, usePathname, useRouter } =
   createSharedPathnamesNavigation({ locales });
