a4e0f51fc7c2bcb6632049bcf12c9a63571e47a6
SeeLey Wang <zdlp@sina.cn>
2024-09-21 10:53:09 +0800

    fix warning dialog style

--- zbook_frontend/src/components/WarningDialog.tsx
+++ zbook_frontend/src/components/WarningDialog.tsx
@@ -23,32 +23,30 @@ export default function WarngingDialog({
               scrollbar-thumb-slate-200 scrollbar-track-slate-100
               dark:scrollbar-thumb-slate-500/50 dark:scrollbar-track-slate-500/[0.16]"
       >
-        <div className="flex lg:block md:overflow-hidden grow  justify-center overflow-auto  items-center">
-          <div className="sm:overflow-hidden sm:rounded-md">
-            <div className="grid grid-cols-6 gap-4 gap-x-8 p-2">
-              <div className="col-span-6 justify-center flex-center">
-                <label
-                  htmlFor="post_title"
-                  className="block text-center pt-2 pb-6 text-2xl font-bold text-gray-700 dark:text-slate-50"
-                >
-                  {title}
-                </label>
-              </div>
-            </div>
-            <div className="grid grid-cols-2 gap-x-4 sm:gap-x-6 lg:gap-x-4 xl:gap-x-6 p-4 sm:px-6 sm:py-5 lg:p-4 xl:px-6 xl:py-5">
-              <button
-                className="text-base font-medium rounded-lg bg-white border-[0.05rem] dark:border-0 border-slate-300/50 text-slate-900 py-3 text-center cursor-pointer dark:bg-gray-700/75 dark:text-slate-200 dark:highlight-white/10"
-                onClick={cancelFunc}
+        <div className="sm:rounded-md">
+          <div className="grid grid-cols-6 gap-4 gap-x-8 p-2">
+            <div className="col-span-6 justify-center flex-center">
+              <label
+                htmlFor="post_title"
+                className="block text-center pt-2 pb-6 text-2xl font-bold text-gray-700 dark:text-slate-50"
               >
-                {cancelTitle}
-              </button>
-              <button
-                className="text-base font-medium rounded-lg bg-sky-500 dark:bg-sky-800 text-white py-3 text-center cursor-pointer dark:highlight-white/20"
-                onClick={submitFunc}
-              >
-                {submitTitle}
-              </button>
+                {title}
+              </label>
             </div>
+          </div>
+          <div className="grid grid-cols-2 gap-x-4 sm:gap-x-6 lg:gap-x-4 xl:gap-x-6 p-4 sm:px-6 sm:py-5 lg:p-4 xl:px-6 xl:py-5">
+            <button
+              className="text-base font-medium rounded-lg bg-white border-[0.05rem] dark:border-0 border-slate-300/50 text-slate-900 py-3 text-center cursor-pointer dark:bg-gray-700/75 dark:text-slate-200 dark:highlight-white/10"
+              onClick={cancelFunc}
+            >
+              {cancelTitle}
+            </button>
+            <button
+              className="text-base font-medium rounded-lg bg-sky-500 dark:bg-sky-800 text-white py-3 text-center cursor-pointer dark:highlight-white/20"
+              onClick={submitFunc}
+            >
+              {submitTitle}
+            </button>
           </div>
         </div>
       </header>
