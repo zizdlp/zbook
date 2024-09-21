222f7de4f1b5b1bc5455f6b9b950f8f4bc6fca22
SeeLey Wang <zdlp@sina.cn>
2024-09-21 12:34:23 +0800

    a

--- zbook_frontend/src/app/[locale]/workspace/[username]/(repos)/o/[reponame]/[...href]/layout.tsx
+++ zbook_frontend/src/app/[locale]/workspace/[username]/(repos)/o/[reponame]/[...href]/layout.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
 import MarkdownLoading from "@/components/loadings/MarkdownLoading";
-import ContentBarLoading from "@/components/loadings/ContentBarLoading";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  return <> {children}</>;
   return (
     <Suspense
       fallback={
