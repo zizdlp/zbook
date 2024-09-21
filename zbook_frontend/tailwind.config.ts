4d9b00ef2cd132ab86cd3f6e01bdd765ce263b72
SeeLey Wang <zdlp@sina.cn>
2024-09-21 17:40:06 +0800

    upads

--- zbook_frontend/tailwind.config.ts
+++ zbook_frontend/tailwind.config.ts
@@ -11,74 +11,74 @@ const config: Config = {
 
   safelist: [
     {
-      pattern: /(bg)-(cyan|green|red|yellow|teal)-(600)\/(75)/,
+      pattern: /(bg)-(cyan|green|red|yellow|teal)-(600)\/(75)$/,
     },
     {
-      pattern: /(bg)-(cyan|green|red|yellow|teal)-(500)\/(25)/,
+      pattern: /(bg)-(cyan|green|red|yellow|teal)-(500)\/(25)$/,
       variants: ["dark"],
     },
     {
-      pattern: /(text)-(yellow|violet|teal|sky|pink|indigo)-(500|600|700|900)/,
+      pattern: /(text)-(yellow|violet|teal|sky|pink|indigo)-(500|600|700|900)$/,
     },
     {
-      pattern: /(text)-(yellow|violet|teal|sky|pink|indigo)-(500|600)/,
+      pattern: /(text)-(yellow|violet|teal|sky|pink|indigo)-(500|600)$/,
       variants: ["hover"],
     },
     {
-      pattern: /(text)-(yellow|violet|teal|sky|pink|indigo)-(400)/,
+      pattern: /(text)-(yellow|violet|teal|sky|pink|indigo)-(400)$/,
       variants: ["dark", "hover:dark"],
     },
     {
-      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(600)/,
+      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(600)$/,
       variants: ["hover:dark"],
     },
     {
-      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(500)/,
+      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(500)$/,
       variants: ["hover"],
     },
     {
-      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(400)/,
+      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(400)$/,
     },
     {
-      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(800)/,
+      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(800)$/,
       variants: ["dark"],
     },
     {
-      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(500)/,
+      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(500)$/,
       variants: ["focus"],
     },
     {
-      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(600)/,
+      pattern: /(border)-(yellow|violet|teal|sky|pink|indigo)-(600)$/,
       variants: ["dark:focus"],
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(400)\/(10)/,
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(400)\/(10)$/,
       variants: ["hover"],
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(400)\/(10)/, //submenufile item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(400)\/(10)$/, //submenufile item
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(500)\/(10)/, //submenufile item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(500)\/(10)$/, //submenufile item
       variants: ["dark"],
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(500)/, //repo sidebar item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(500)$/, //repo sidebar item
       variants: ["group-hover"],
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(600)/, //repo sidebar item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(600)$/, //repo sidebar item
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(700)/, //repo sidebar item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(700)$/, //repo sidebar item
       variants: ["hover"],
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(700)\/(50)/, //repo sidebar item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(700)\/(50)$/, //repo sidebar item
       variants: ["dark"],
     },
     {
-      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(800)\/(50)/, //repo sidebar item
+      pattern: /(bg)-(yellow|violet|teal|sky|pink|indigo)-(800)\/(50)$/, //repo sidebar item
       variants: ["hover:dark"],
     },
   ],
